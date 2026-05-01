394d0ae3d3e683ad6ee2334a1cbdfff89b102ca4
Gelisama Ndraha <agusraha66@gmail.com>
2026-05-01 19:20:24 +0700

    run build

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,9 @@ export default defineConfig({
             ignored: ['**/storage/framework/views/**'],
         },
     },
+
+      build: {
+        manifest: true,
+        outDir: 'public/build',
+    },
 });
